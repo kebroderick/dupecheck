d2bd6be48e628d7214a612991f0d90563d25a3a6
kebroderick <kevin.broderick@awacservices.com>
2023-09-18 15:39:36 +0100

    Adding dupecheck logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Looking for duplicate files..."
 dupeFiles=$(find . | tr '[A-Z]' '[a-z]' | sort | uniq -c | grep -v " 1 ")
 dupeCount=$(find . | tr '[A-Z]' '[a-z]' | sort | uniq -c | grep -v " 1 " | wc -l)
 
-if [ $dupeCount -ge 0 ]; then
+if [ $dupeCount -gt 0 ]; then
 	echo ERROR: Duplicates detected:
 	echo $dupeFiles | tr " " "\n"
 	exit 1 # terminate and indicate error
